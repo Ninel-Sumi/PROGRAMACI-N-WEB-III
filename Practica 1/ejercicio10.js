3be4b4160bc6527d2cc50531b639897e96683bc4
Ninel-Sumi <ninelsumi06@gmail.com>
2026-03-24 22:18:32 -0300

    Update ejercicio10.js

---
+++ Practica 1/ejercicio10.js
@@ -0,0 +1,2 @@
+//Callback: conveniente cuando la operación es sencilla y no necesitas encadenar múltiples operaciones asíncronas.
+//Promesa: necesaria cuando quieres manejar varias operaciones asíncronas, encadenarlas, manejar errores de forma más clara o usar async/await.
